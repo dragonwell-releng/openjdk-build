6c84f6a0c5056f2f9a12a79af98092f8214ed5aa
lvfei.lv <lvfei.lv@alibaba-inc.com>
2025-09-04 18:30:23 +0800

    dw21 adapt aiext build

--- sbin/build.sh
+++ sbin/build.sh
@@ -141,6 +141,9 @@ configureMacOSCodesignParameter() {
   if [[ "${BUILD_CONFIG[BUILD_VARIANT]}" == "${BUILD_VARIANT_DRAGONWELL}" ]] && [ "${BUILD_CONFIG[OPENJDK_CORE_VERSION]}" == "jdk21" ]; then
     #local macos_bundle_build_version=0
     addConfigureArg "--with-macosx-bundle-build-version=" "0"
+    if [ "${BUILD_CONFIG[OS_KERNEL_NAME]}" == "linux" ]; then
+      addConfigureArg "--with-jvm-features=" "aiext"
+    fi
   fi
 }
 
@@ -785,6 +788,13 @@ executeTemplatedFile() {
     mkdir -p ${PRODUCT_HOME}/jre/lib/${arch_dir}/serverless
     cp serverless-adapter/target/serverless-adapter-0.1.jar ${PRODUCT_HOME}/jre/lib/${arch_dir}/serverless/serverless-adapter.jar
     cp serverless-adapter/output/libloadclassagent.so ${PRODUCT_HOME}/jre/lib/${arch_dir}/serverless
+  elif [ "${BUILD_CONFIG[OPENJDK_CORE_VERSION]}" == "jdk21" ] && [ -n "`echo ${BUILD_CONFIG[BUILD_FULL_NAME]} | grep -E 'linux-x86_64|linux-aarch64'`" ]; then
+    PRODUCT_HOME=$(ls -d ${PWD}/build/*/images/${BUILD_CONFIG[JDK_PATH]})
+    libjvm_path=$(find ${PRODUCT_HOME} -iname libjvm.so)
+    wget https://dragonwell.oss-cn-shanghai.aliyuncs.com/tools/signutil -O signutil
+    chmod +x signutil
+    ./signutil sign ~/sk.bin ${libjvm_path}
+    ./signutil verify ~/pk.bin ${libjvm_path}
   fi
 }
 
